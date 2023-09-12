21bbb104617b06e972c902807f1426950635aa3b
sdschindler <sdschindler@yahoo.com>
2023-09-12 14:14:19 -0500

    Add files via upload

---
+++ mpl_squares.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from matplotlib import interactive
+interactive(True)
+input_values = [1,2,3,4,5]
+squares = [1,4,9,16,25]
+plt.plot(input_values,squares, linewidth=5)
+
+plt.title("square Numbrs", fontsize = 24)
+plt.xlabel("value",fontsize = 14)
+plt.ylabel("squar of Value",fontsize = 14)
+
+plt.tick_params(axis ='both', labelsize = 14)
+plt.show(block=True)
